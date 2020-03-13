85dc65e6d9f46d1f045e1285528586c07227f6c1
Oluwatito Ebiwonjumi <titoebiwonjumi@gmail.com>
2020-03-12 23:53:28 -0400

    Testing Script

--- updateGit.sh
+++ updateGit.sh
@@ -3,4 +3,7 @@
 git add .
 read -p 'Commit Message: ' commitMessage
 git commit -m  "$commitMessage"
+echo "Pushing to Git"
 git push  origin master
+echo "Pushing to Heroku"
+git push heroku master
