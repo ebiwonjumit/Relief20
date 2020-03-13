1076149d54ece44842dca91e3f292d71bd26278d
Oluwatito Ebiwonjumi <titoebiwonjumi@gmail.com>
2020-03-13 00:08:16 -0400

    Hopefully finally works Redesign

--- updateGit.sh
+++ updateGit.sh
@@ -3,9 +3,9 @@
 git add .
 read -p 'Commit Message: ' commitMessage
 git commit -m  "$commitMessage"
-echo "\n\n\n..............................................................."
-echo "Pushing to Git\n"
+echo -e "\n\n\n..............................................................."
+echo -e "Pushing to Git\n"
 git push  origin master
-echo "\n\n\n..............................................................."
-echo "Pushing to Heroku\n"
+echo -e "\n\n\n..............................................................."
+echo -e "Pushing to Heroku\n"
 git push heroku master
